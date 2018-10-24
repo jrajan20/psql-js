cee1ca3b74fcc4e039f429a610ab22585dcbda41
jrajan20 <jai_20j@hotmail.com>
2018-10-24 00:07:29 +0000

    milestones migration added

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+const settings = require("./settings");
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+       user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+}
+  },
+
+  staging: {
+    client: 'postgresql',
+   connection: {
+       user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+},
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+     connection: {
+       user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+},
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
